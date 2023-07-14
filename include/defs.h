ba2108b947128e3f97270ab9a6ad1a8508b2063d
unknown <analuiza.miscoli@estudante.ufjf.br>
2023-07-14 16:55:41 -0300

    Atualização a partir de testes

--- include/defs.h
+++ include/defs.h
@@ -11,16 +11,16 @@ int TXPin = 17;
 
 //pinos do LoRa
 #define csPin 5  // Chip Select ( Slave Select do protocolo SPI ) do modulo Lora
-#define resetPin 4 // Reset do modulo LoRa
-#define irqPin 2   // Pino DI0
+#define resetPin 2 // Reset do modulo LoRa
+#define irqPin 4   // Pino DI0
 
 #define BAND 915E6 // Frequência do módulo LoRa
 
 // pinos SPI do módulo SD
-#define SCK_PIN 18
-#define MISO_PIN 19
-#define MOSI_PIN 23
-#define CS_PIN 5
+#define SCK_PIN 14
+#define MISO_PIN 12
+#define MOSI_PIN 13
+#define CS_PIN 15
 
 #define P0 1013.25 // pressão atmosférica média no nível do mar caso precise, mas o bmp.readAltitude ja possui o parâmetro float seaLevelhPa = 1013.25 na documentação
 
